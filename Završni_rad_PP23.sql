ca6407938d2da57b71e3beb3ce68629ce1c7b0d4
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-05-30 15:58:57 +0200

    napravljena tablica operacije

--- Završni_rad_PP23.sql
+++ Završni_rad_PP23.sql
@@ -3,7 +3,7 @@ create database zavrsni_rad_PP23;
 use zavrsni_rad_PP23;
 
 create table opg(
-	sifra int not null primary key auto_increment not null,
+	sifra int not null primary key auto_increment,
 	mipbg varchar (11) not null,
 	adresa varchar (50),
 	mjesto varchar (50) not null,
@@ -12,4 +12,15 @@ create table opg(
 	kultura int not null,
 	poljopricredno_zemljiste int not null,
 	mehanizacija int not null
+);
+
+create table operacija(
+	sifra int not null primary key auto_increment;
+	naziv varchar(50) not null,
+	mehanizacija int not null,
+	poljoprivredno_zemljiste int not null,
+	kultura int not null,
+	sredstvo varchar(25),
+	pocetak datetime not null,
+	kraj datetime
 );
