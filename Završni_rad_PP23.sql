28b7bcf05677176dea65e62e46302725cc631e23
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-05-30 15:23:02 +0200

    napravljena tablica opg

--- Završni_rad_PP23.sql
+++ Završni_rad_PP23.sql
@@ -2,3 +2,14 @@ drop database if exists zavrsni_rad_PP23;
 create database zavrsni_rad_PP23;
 use zavrsni_rad_PP23;
 
+create table opg(
+	sifra int not null primary key auto_increment not null,
+	mipbg varchar (11) not null,
+	adresa varchar (50),
+	mjesto varchar (50) not null,
+	postanski_broj char (5),
+	radnici int not null,
+	kultura int not null,
+	poljopricredno_zemljiste int not null,
+	mehanizacija int not null
+);
