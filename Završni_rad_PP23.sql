dc839288a7ed516c487510d0efe6cdb72df1ed8f
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-09-27 17:34:24 +0200

    početak rada na završnom
    čeka me pun klinac posla

--- Završni_rad_PP23.sql
+++ Završni_rad_PP23.sql
@@ -8,11 +8,19 @@ create table opg(
 	adresa varchar (50),
 	mjesto varchar (50) not null,
 	postanski_broj char (5),
-	radnici int not null,
-	kultura int not null,
-	poljoprivredno_zemljiste int not null,
-	mehanizacija int not null
+	radnici int,
+	kultura int,
+	poljoprivredno_zemljiste int,
+	mehanizacija int
 );
+
+
+insert into opg(email,lozinka,ime,prezime,uloga) values 
+('admin@edunova.hr','$2y$10$sc61AG4pNc2ddh4SVRYr1.VVaH58j8LRf03QkUwrC1AKN95acN.qu',
+'Administrator','Edunova','admin'),
+('oper@edunova.hr','$2y$10$sc61AG4pNc2ddh4SVRYr1.VVaH58j8LRf03QkUwrC1AKN95acN.qu',
+'Operater','Edunova','oper');
+
 
 create table operacija(
 	sifra int not null primary key auto_increment,
@@ -58,15 +66,20 @@ create table mehanizacija(
 	tip_tla int not null
 );
 
+create table radnici(
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	uloga varchar(50) not null,
+	nivo obrazovanja varchar (50)
+);
+
 
 alter table opg add foreign key (poljoprivredno_zemljiste) references poljoprivredno_zemljiste (sifra);
 alter table poljoprivredno_zemljiste add foreign key (tip_tla) references tip_tla (sifra);
 alter table tip_tla add foreign key (poljoprivredno_zemljiste) references poljoprivredno_zemljiste (sifra);
 alter table mehanizacija add foreign key (operacija) references operacija (sifra);
 alter table mehanizacija add foreign key (tip_tla) references tip_tla (sifra);
-
-
-
 
 
 
