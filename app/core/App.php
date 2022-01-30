30c7236714f934d28f50d192b98b7f1a191d6141
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-01-31 00:44:11 +0100

    app

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,46 @@ class App
 {
     public static function start()
     {
+        $route = Request::getRoute();
         
+        $parts = explode('/',$route);
+        
+        $class = '';
+        if(!isset($parts[1]) || $parts[1]==''){
+            $class = 'Index';
+        }else{
+            $class = ucfirst($parts[1]);
+        }
+        $class .= 'Controller';
+
+        $method = ''
+        if(!isset($parts [2]) || $parts [2]==''){
+            $method = 'index';
+        }else{
+            $method=$parts[2];
+        }
+        $code = 0;
+        if(!isset($parts [3]) || $parts [4]==''){
+        }else{
+            $code[3];
+        }
+        if(class_exists($class) && method_exists($class,$method)){
+            $instance = new $class();
+            if($code==0){
+                $instance->$method();
+            }else{
+                $instance->$method($code);
+            }
+        }else{
+            $view = new View();
+            $view-render ('error404',[
+                'ERROR'=>$class . '->' . $method
+            ]);
+        }
+    }
+    public static function config($key)
+    {
+        $config = include BP_APP . 'configuration.php';
+        return $config[$key];
     }
 }
