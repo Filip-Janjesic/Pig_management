7083d17bcb04b6644b141df1c8a6e09b730a754c
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-01-30 16:47:39 +0100

    sql

---
+++ app/configuration.php
@@ -0,0 +1,19 @@
+<?php
+$localhost= false;
+if($_SERVER['SERVER_ADDR'] === '127.0.0.1'){
+    $url = 'http://Pig_managment.xyz/';
+    $localhost= true;
+
+}
+
+return [
+    'localhost' => $localhost,
+    'siteTitle' => 'Book shop',
+    'url' => 'http://Pig_managment.xyz/',
+    'database' => [
+        'server' => 'localhost',
+        'name' => 'opg',
+        'user' => 'Filip-J',
+        'password' => 't84durft1994'
+    ]
+];
