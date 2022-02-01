350d82eba2fa7bc89f290cfb26aa73685e721469
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-02-01 23:35:43 +0100

    request

---
+++ app/core/Request.php
@@ -0,0 +1,31 @@
+<?php
+
+class Request
+{
+    public static function getRoute()
+    {
+        $route ='/';
+        if(isset˙($_SERVER['REDIRECT_PATH_INFO'])){
+            $route = $_SERVER['REDIRECT_PATH_INFO'];
+        }else if (isset($_SERVER['REQUEST_URI'])){
+            $route = $_SERVER['REQUEST_URI'];
+        }
+        return $route
+    }
+
+    public static function isAuthorized()
+    {
+        return isset($_SESSION['authorized']);
+    }
+
+    public static function user()
+    {
+        return $_SESSION['authorized']->username . ' ' 
+                . $_SESSION['authorized']->name;
+    }
+
+    public static function isAdmin()
+    {
+        return $_SESSION['authorized']->name==='admin';
+    }
+}
