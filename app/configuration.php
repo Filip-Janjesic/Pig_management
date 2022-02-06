bb9f3a444802871bf77431ed166f3c6dee67acb5
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-02-06 22:34:35 +0100

    izmjene

--- app/configuration.php
+++ app/configuration.php
@@ -8,7 +8,7 @@ if($_SERVER['SERVER_ADDR'] === '127.0.0.1'){
 
 return [
     'localhost' => $localhost,
-    'nameAPP' => '',
+    'nameAPP' => 'Pig managment',
     'url' => 'http://Pig_managment.xyz/',
     'database' => [
         'server' => 'localhost',
